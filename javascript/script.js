445a23a31dafcb1af82c3ec114aa9b2d8e915f6e
Istiaq Abdullah <abdullahistiaq77@gmail.com>
2026-03-31 15:20:55 +0100

    data types

---
+++ javascript/script.js
@@ -0,0 +1,33 @@
+let age = 20;
+const name = "Istiaq";
+
+console.log(age);
+console.log(name);
+
+age = 21;
+console.log(age);
+
+
+let greeting ="Hello, there";
+console.log(greeting);
+
+let price = 4.99;
+console.log(price);
+
+let isStudent = true;
+console.log(isStudent);
+
+let colours = ["red", "blue", "green"];
+console.log(colours);
+console.log(colours[0]);
+console.log(colours[2]);
+
+let person = {
+    name: "Istiaq",
+    age: 20,
+    isStudent: true
+};
+
+console.log(person);
+console.log(person.name);
+console.log(person.age);
