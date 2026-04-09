16442b3205806bef8d564b3bb3730bf4c8fbc3c4
Istiaq Abdullah <abdullahistiaq77@gmail.com>
2026-04-09 08:39:45 +0100

    Declare a const called person as an object with a name and age. Log a sentence using both values.

---
+++ e1.js
@@ -0,0 +1,10 @@
+//Declare a const called person as an object with a name and age. Log a sentence using both values.
+
+const person = {
+    name: "Istiaq",
+    age: 20
+};
+
+console.log(person.name);
+console.log(person.age);
+
