6ef6a60238791226bb3b2cb15f5f408ef5107fae
Istiaq Abdullah <abdullahistiaq77@gmail.com>
2026-04-09 12:49:38 +0100

    Write a function that takes a number and returns "even" or "odd" using a ternary.

---
+++ e2.js
@@ -0,0 +1,6 @@
+//Write a function that takes a number and returns "even" or "odd" using a ternary.
+const isEvenorOdd = (num) => num % 2 === 0 ? "even" : "odd";
+
+console.log(isEvenorOdd(4));
+console.log(isEvenorOdd(7));
+console.log(isEvenorOdd(0));
