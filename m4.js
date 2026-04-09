a8d2c9d70de282ff850a5c110f2e379d98f75299
Istiaq Abdullah <abdullahistiaq77@gmail.com>
2026-04-09 13:08:05 +0100

    Write an arrow function called gradeScore that takes a number. Return "A" (90+), "B" (70+), "C" (50+), or "Fail" below 50. Test it with 4 different values.

---
+++ m4.js
@@ -0,0 +1,17 @@
+//4Write an arrow function called gradeScore that takes a number. Return "A" (90+), "B" (70+), "C" (50+), or "Fail" below 50. Test it with 4 different values.
+const gradeScore = (grade) => {
+    if(grade >=90){
+        return "A";
+    } else if (grade>=70){
+        return "B";
+    } else if (grade>=50){
+       return "C";
+    } else{
+        return "Fail";
+    }
+};
+
+console.log(gradeScore(98));
+console.log(gradeScore(23));
+console.log(gradeScore(50));
+console.log(gradeScore(-1));
