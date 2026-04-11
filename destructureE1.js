d5277e761cd63f36d460f8aae5e64f776a2972ff
Istiaq Abdullah <abdullahistiaq77@gmail.com>
2026-04-11 19:30:09 +0100

    Destructure title, year, and director from this movie object and log each one.

---
+++ destructureE1.js
@@ -0,0 +1,9 @@
+//Destructure title, year, and director from this movie object and log each one.
+
+const movie = {title:"Inception",year:2010,director:"Nolan",rating:9};
+
+const {title, year, director, rating} = movie;
+console.log(title);
+console.log(year);
+console.log(director);
+
